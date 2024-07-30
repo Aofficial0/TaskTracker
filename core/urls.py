c00d94a463f0279ec91f29a419304e220e4f7b01
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-07-30 14:23:09 +0000

    added a task detail class in views to get the details of a task and linked it to urlpatterns in urls and created a html template for task detail

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import  TaskTrack
+from .views import  TaskTrack, TaskDetail
 
 urlpatterns = [
     path('', TaskTrack.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
 class TaskTrack(ListView):
     model = Task 
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
