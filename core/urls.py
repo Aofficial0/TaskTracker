402a207c68da3aa3bbab272bb23f855561a6a8f4
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-07-31 12:41:31 +0000

    added a task create class to views to start creating new tasks with model and fields and imported it to the urls and did a path to task create and added a task form template with a html form and csrf token and added a link to add the task in the task list template

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import  TaskTrack, TaskDetail
+from .views import  TaskTrack, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskTrack.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
@@ -13,3 +16,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name= 'task'
     template_name= 'core/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
