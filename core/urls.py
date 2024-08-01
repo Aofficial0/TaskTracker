b6f5117b0d7dd98418d9602e65ac68ad378b7953
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-01 09:38:22 +0000

    in views imprted DeleteView and added a DeleteView class with model and context object name and linked that to the right path in url and created a new task confirm delete template with a confirmation message in it and csrf token and a go back to tasks button

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate
+from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskTrack.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
     
 ]
