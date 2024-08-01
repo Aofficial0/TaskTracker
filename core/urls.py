66794fb9209250cdfb5b35bc68973a5e6020fb2b
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-01 15:20:34 +0000

    imported loginview in views.py and added class with template name and fields and redirect link and in task list template added links for loging in and loging out in an if else condition and created a new template for login and added csrf token  in it and a from method post with a button to submit

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskTrack.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- core/views.py
+++ core/views.py
@@ -4,9 +4,21 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'core/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
 class TaskTrack(ListView):
     model = Task 
     context_object_name = 'tasks'
