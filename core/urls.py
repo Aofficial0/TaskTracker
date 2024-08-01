f7c7aceaa076db57379cdb33270e52babf54e4f8
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-01 08:56:51 +0000

    Added an update view to views and added a task update class with model and fields and added the rigth path in urls for task update and un task list html template added a link for the editing in the table so user can edit the tasks

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import  TaskTrack, TaskDetail, TaskCreate
+from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskTrack.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    
 ]
