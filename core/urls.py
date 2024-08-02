82ed72122e32bfcfd3fb67a10c7bd0b4b841f64f
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-02 15:38:30 +0000

    Add user registration functionality with authentication and auto-login and Implement RegisterPage class-based view in views.py and Create 'core/register.html' template for user registration form and Add form validation to save new user and log them in automatically

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from .views import  TaskTrack, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 
     path('', TaskTrack.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 from .models import Task
 
@@ -20,6 +22,23 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'core/register.html'
+    form_class = UserCreationForm 
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get( *args, **kwargs)
 
 class TaskTrack(LoginRequiredMixin, ListView):
     model = Task 
