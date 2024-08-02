fdaddf861dcf263800b3ed221ee53bcf35a20600
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-02 14:27:05 +0000

    added get_context_data to views in the track task class and added a function in same class to make sure each task set goes to the user logged in and fixed task create and task update fields only to show title , description and complete so users can not select or see other users that exist on the database

--- core/views.py
+++ core/views.py
@@ -25,6 +25,11 @@ class TaskTrack(LoginRequiredMixin, ListView):
     model = Task 
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -33,13 +38,16 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task 
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin, DeleteView):
