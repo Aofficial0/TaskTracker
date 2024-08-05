43b4cd3a4ce350d0f4442b10f0fadde2f0282305
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-08-05 14:32:07 +0000

    fixed the database code and linked to env.py

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -83,19 +83,20 @@ WSGI_APPLICATION = 'tasktracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL", "postgres://u0p7fmueuvj:dEWGxAZmr9TB@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/evoke_path_slum_257886")
-    )
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
